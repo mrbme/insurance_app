002ca10aea2a1dfab002e9ee085f1471e3251ba5
Blake Epstein <blake@brightforestmedia.com>
2018-07-26 13:10:50 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :prospects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'page2' => "home#page2"
 end
